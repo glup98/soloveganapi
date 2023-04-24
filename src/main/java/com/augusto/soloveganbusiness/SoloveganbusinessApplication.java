12337f293bbf7eba16dd23634e8c38138fa6576f
Augusto Mendoza <augustomendoza1998@gmail.com>
2023-04-23 22:49:32 -0400

    starting solovegan_business

---
+++ src/main/java/com/augusto/soloveganbusiness/SoloveganbusinessApplication.java
@@ -0,0 +1,13 @@
+package com.augusto.soloveganbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloveganbusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloveganbusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/augusto/soloveganbusiness/SoloveganbusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augusto.soloveganbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloveganbusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
